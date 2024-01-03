0767e60774fd5a8340bf4d735be1eb7f0852ddf1
Gamal <jrjb079@gmail.com>
2024-01-03 21:32:44 +0200

    - adding required dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.9.21" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
+    id("com.google.devtools.ksp") version "1.9.21-1.0.15" apply false
+}
+buildscript {
+    dependencies {
+        classpath(kotlin("gradle-plugin", version = "1.9.21"))
+        val navVersion = "2.7.6"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
+    }
 }
